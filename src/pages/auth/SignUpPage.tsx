8e3bb23a68fb40ab9c84318b59c8dba5e71d0ec9
tsihnavy99 <tsihnavy99@gmail.com>
2025-02-09 03:44:32 +0900

    add SignupPage (#9)
    SignupPage 추가 및 기본 UI 구성, signupSchma 추가

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,10 +1,73 @@
+import InputContainer from '@/components/InputContainer';
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Form, FormField } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { SignupFormValues, signupSchma } from '@/schemas/auth.schma';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 const SignUpPage: FC = () => {
+  const form = useForm<SignupFormValues>({
+    resolver: zodResolver(signupSchma),
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmit = async (values: SignupFormValues) => {
+    console.log('회원가입', values);
+  };
+
   return (
-    <div>
-      <h1>회원가입 페이지</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>회원가입</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            {/* email 입력 */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <InputContainer label="E-mail">
+                  <Input placeholder="id@example.com" type="email" {...field} />
+                </InputContainer>
+              )}
+            />
+
+            {/* 비밀번호 입력 */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <InputContainer label="Password">
+                  <Input placeholder="********" type="password" {...field} />
+                </InputContainer>
+              )}
+            />
+
+            {/* 비밀번호 확인인 */}
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <InputContainer label="Confirm Password">
+                  <Input placeholder="********" type="password" {...field} />
+                </InputContainer>
+              )}
+            />
+
+            <Button type="submit">회원가입</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
--- src/schemas/auth.schma.ts
+++ src/schemas/auth.schma.ts
@@ -5,4 +5,16 @@ export const loginSchema = z.object({
   password: z.string().min(8, '비밀번호는 최소 8자 이상이어야 합니다').max(100, '비밀번호가 너무 깁니다'),
 });
 
+export const signupSchma = z
+  .object({
+    email: z.string().email('올바른 이메일을 입력해주세요'),
+    password: z.string().min(8, '비밀번호는 최소 8자 이상이어야 합니다').max(100, '비밀번호가 너무 깁니다'),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: '비밀번호가 일치하지 않습니다',
+    path: ['confirmPassword'],
+  });
+
 export type LoginFormValues = z.infer<typeof loginSchema>;
+export type SignupFormValues = z.infer<typeof signupSchma>;
